587039dbd2b283732192517bac0ed1db9390d865
Eishu Kawaguchi <sikewa-e.k@outlook.jp>
2024-06-25 23:46:35 +0900

    create: scrape func

--- go.mod
+++ go.mod
@@ -1,5 +1,19 @@
-module go-pdfxtractor
+module github.com/Acu4git/go-pdfxtractor
 
-go 1.22.1
+go 1.22.4
 
-require github.com/unidoc/unipdf/v3 v3.56.0 // indirect
+require github.com/pdfcpu/pdfcpu v0.8.0
+
+require (
+	github.com/PuerkitoBio/goquery v1.9.2 // indirect
+	github.com/andybalholm/cascadia v1.3.2 // indirect
+	github.com/hhrutter/lzw v1.0.0 // indirect
+	github.com/hhrutter/tiff v1.0.1 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	golang.org/x/image v0.17.0 // indirect
+	golang.org/x/net v0.24.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,76 +3,79 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
-	"strings"
-	"time"
+	"regexp"
 
-	"github.com/unidoc/unipdf/v3/common/license"
+	"github.com/PuerkitoBio/goquery"
 )
 
-func init() {
-	apiKey := os.Getenv("UNIPDF_API_KEY")
-	err := license.SetMeteredKey(apiKey)
+// 指定したURL(url)のレスポンスから，検索パターン(pattern)に合致するURLを1つ返す
+func scrape(url, pattern string) (string, error) {
+	// スクレイピング対象のページにリクエストを送る
+	res, err := http.Get(url)
 	if err != nil {
-		panic(err)
+		return "", fmt.Errorf("failed to fetch the URL: %v", err)
 	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("failed to fetch the URL: status code %d", res.StatusCode)
+	}
+
+	// goqueryドキュメントを作成
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		return "", fmt.Errorf("failed to parse the HTML: %v", err)
+	}
+
+	// 検索パターン(pattern)をGoの正規表現としてコンパイル
+	re := regexp.MustCompile(pattern)
+
+	// リンクを探す
+	found := false
+	doc.Find("a").Each(func(index int, item *goquery.Selection) {
+		href, exists := item.Attr("href")
+		if exists && re.MatchString(href) {
+			fmt.Printf("found link: %s\n", href)
+			url = href
+			found = true
+		}
+	})
+
+	if !found {
+		return "", fmt.Errorf("not found link")
+	}
+
+	return url, nil
 }
 
 func main() {
-	pdfURL := "https://www.kit.ac.jp/wp/wp-content/uploads/2024/04/R6hpsyougakukinitiran20240408.pdf"
-
-	filename := getFilename(pdfURL)
-
-	err := downloadFile(pdfURL, filename)
+	// スクレイピング
+	url := "https://www.kit.ac.jp/campus_index/life_fee/scholarship/minkanscholarship/"
+	linkPattern := `https://www\.kit\.ac\.jp/wp/wp-content/uploads/\d{4}/\d{2}/.*hpsyougakukinitiran.*\.pdf`
+	url, err := scrape(url, linkPattern)
 	if err != nil {
-		fmt.Println("Error downloading file:", err)
-		return
+		log.Fatal(err)
 	}
-	fmt.Println("File downloaded successfully")
 
-	time.Sleep(time.Second * 5)
-	if err = deleteFile(filename); err != nil {
-		fmt.Println("Error deleting file:", err)
-	}
-}
-
-func getFilename(path string) string {
-	pathElems := strings.Split(path, "/")
-	return pathElems[len(pathElems)-1]
-}
-
-func downloadFile(url string, filename string) error {
-
-	resp, err := http.Get(url)
+	// PDFレスポンスを受け取る
+	res, err := http.Get(url)
 	if err != nil {
-		return err
+		log.Fatal(err)
 	}
-	defer resp.Body.Close()
+	defer res.Body.Close()
 
-	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("unexpected status code: %s", resp.Status)
+	if res.StatusCode != http.StatusOK {
+		log.Fatalf("failed to fetch the URL: status code %d", res.StatusCode)
 	}
 
-	outFile, err := os.Create(filename)
+	// 一旦PDFファイルをローカルに保存
+	f, err := os.Create("input.pdf")
 	if err != nil {
-		return err
+		log.Fatal(err)
 	}
-	defer outFile.Close()
+	io.Copy(f, res.Body)
 
-	_, err = io.Copy(outFile, resp.Body)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func deleteFile(filename string) error {
-	err := os.Remove(filename)
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
